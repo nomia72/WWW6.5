3e4030d6629ec992d4cfbde264703b08db1fda54
QWQ <wangq814@newschool.edu>
2026-03-13 02:07:41 +0800

    day8 nomia

--- nomia/Day8-TipJar.sol
+++ nomia/Day8-TipJar.sol
@@ -2,4 +2,17 @@
 
 pragma solidity ^0.8.0;
 
+contract TipJar{
+    address public owner;
+    string[] public supportedCurrencies;
+    mapping(string => uint256)conversionRate;
+    uint public totalTipsRecieved;
+    
 
+
+
+
+
+
+
+}
