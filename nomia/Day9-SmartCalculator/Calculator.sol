93cfd14fb5cf0acb7a7f26a9fedc6e66991ad845
QWQ <wangq814@newschool.edu>
2026-03-15 02:01:27 +0800

    update day 9+10

--- nomia/Day9-SmartCalculator/Calculator.sol
+++ nomia/Day9-SmartCalculator/Calculator.sol
@@ -20,7 +20,7 @@ contract Calculator{
 
     }
 
-    function setScientificCalculator(address _address)public onlyOwner{
+    function setScientificCalculator(address _address)public onlyOwner{ //owner设置ScientificCalculator的address
         ScientificCalculatorAddress = _address;
 
 
@@ -50,19 +50,20 @@ contract Calculator{
 
     }
 
+//调用ScientificCalculator的power（）
     function calculatePower(uint256 base, uint256 exponent)public view returns(uint256){   
         ScientificCalculator scientificCalc =  ScientificCalculator(ScientificCalculatorAddress);
         //external call 
-        uint256 result = scientificCalc.power(base,exponent);
+        uint256 result = scientificCalc.power(base,exponent); //直接调用
         return result;
     }
 
-    function calculateSquareRoot(uint256 number)public returns(uint256){
+    function calculateSquareRoot(int256 number)public returns(int256){//int 有符号整数
         require(number >= 0, "no negative number here");
-        bytes memory data = abi.encodeWithSignature("squareRoot(int256)",number);
-        (bool success, bytes memory returnData) = ScientificCalculatorAddress.call(data);
+        bytes memory data = abi.encodeWithSignature("squareRoot(int256)",number); //把函数名和参数打包成调用信息
+        (bool success, bytes memory returnData) = ScientificCalculatorAddress.call(data); //低级调用
         require(success, "external call failed");
-        uint256 result = abi.decode(returnData,(uint256));
+        int256 result = abi.decode(returnData,(int256));
         return result;
 
 
